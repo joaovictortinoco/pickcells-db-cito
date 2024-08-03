b003ab4b93c830010ddfafc7178a27589d1c6de5
joaovictortinoco <joaovictortinoco@gmail.com>
2024-08-03 15:59:08 -0300

    Add verification of categories in report

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,60 +7,102 @@ from PIL import Image
 import datetime
 from utils import config
 
+def sum_categories(data):
+    category_sums = {'HSIL': 0, 'LSIL': 0, 'NORMAL': 0}
+    for entry in data:
+        for category, value in entry:
+            category_sums[category] += value
+    
+    return category_sums
+
+def calculate_rates_opt1(hsil, lsil, normal):
+    hsil_rate = hsil / (hsil+lsil+normal)
+    lsil_rate = lsil / (hsil+lsil+normal)
+    normal_rate = normal / (hsil + lsil + normal)
+    print(f'HSIL:{hsil_rate} \n LSIL: {lsil_rate} \n NORMAL: {normal_rate}')
+    if hsil_rate >= 0.40 or ((hsil_rate + normal_rate > 0.6) and hsil_rate > 0.15):
+        final_report_container.warning("Interpretação e Resultado: Possível malignidade HSIL")
+        final_report_container.warning("Complexidade: Alta complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 4")
+    elif lsil_rate >= 0.35 or ((lsil_rate + normal_rate > 0.6) and lsil_rate > 0.15):
+        final_report_container.warning("Interpretação e Resultado: Possível malignidade LSIL")
+        final_report_container.warning("Complexidade: Baixa complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 3")
+    elif normal_rate > 0.6:
+        final_report_container.warning("Interpretação e Resultado: Possível NEGATIVO")
+        final_report_container.warning("Complexidade: Baixa Complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 1 e 2")
+
+def calculate_rates_opt2(hsil, lsil, normal):
+    hsil_rate = hsil / (hsil+lsil)
+    lsil_rate = lsil / (hsil+lsil)
+    normal_rate = normal / (hsil + lsil + normal)
+    print(f'HSIL:{hsil_rate} \n LSIL: {lsil_rate} \n NORMAL: {normal_rate}')
+
+    if hsil_rate >= 0.40:
+        final_report_container.warning("Interpretação e Resultado: Possível malignidade HSIL")
+        final_report_container.warning("Complexidade: Alta complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 4")
+    elif lsil_rate >= 0.35:
+        final_report_container.warning("Interpretação e Resultado: Possível malignidade LSIL")
+        final_report_container.warning("Complexidade: Baixa complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 3")
+    else:
+        final_report_container.warning("Interpretação e Resultado: Possível NEGATIVO")
+        final_report_container.warning("Complexidade: Baixa Complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 1 e 2")
+
+def calculate_rates_opt3(hsil, lsil, normal):
+    hsil_rate_prevalence = hsil / (hsil+lsil)
+    lsil_rate_prevalence = lsil / (hsil+lsil)
+    normal_rate_prevalence = normal / (hsil + lsil + normal)
+    hsil_rate_general = hsil / (hsil+lsil+normal)
+    lsil_rate_general = lsil / (hsil+lsil+normal)
+    
+    print(f'HSIL:{hsil_rate_prevalence} \n LSIL: {lsil_rate_prevalence} \n NORMAL: {normal_rate_prevalence}')
+
+    hsil_final_rate = hsil_rate_prevalence + hsil_rate_general / normal_rate_prevalence
+    lsil_final_rate = lsil_rate_prevalence + lsil_rate_general / normal_rate_prevalence
+    
+    print(hsil_final_rate, lsil_final_rate)
+    
+    if hsil_final_rate > lsil_final_rate:
+        final_report_container.warning("Interpretação e Resultado: Possível malignidade HSIL")
+        final_report_container.warning("Complexidade: Alta complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 4")
+    elif lsil_final_rate > normal_rate_prevalence:
+        final_report_container.warning("Interpretação e Resultado: Possível malignidade LSIL")
+        final_report_container.warning("Complexidade: Baixa complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 3")
+    else:
+        final_report_container.warning("Interpretação e Resultado: Possível NEGATIVO")
+        final_report_container.warning("Complexidade: Baixa Complexidade")
+        final_report_container.warning("Sugestivo: GRUPO 1 e 2")
+
 def analyze_classes_from_prediction(report, final_report: bool | None = False):
-    hsil = 0
-    lsil = 0
-    normal = 0
-    print('========================REPORT========================\n', report, '\n')
+    print('\n\n\n========================REPORT========================\n\n\n', report, '\n')
     if final_report:
-        for r in report:
-            for classes in r:
-                for cell in classes:
-                    if cell == 0:
-                        hsil += 1
-                    elif cell==1:
-                        lsil += 1
-                    else:
-                        normal += 1
         end_time_analysis = datetime.datetime.now()
+        classes_amount = sum_categories(report)
         overall_time = int((end_time_analysis-init_time_analysis).total_seconds()/60)
         if overall_time > 0:
             final_report_container.write(f'Tempo decorrido para análise da lâmina: {int((end_time_analysis-init_time_analysis).total_seconds()/60)} minuto(s)')
         else:
             final_report_container.write(f'Tempo decorrido para análise da lâmina: {int((end_time_analysis-init_time_analysis).total_seconds())} segundos')
         final_report_container.write("Adequação amostra: Satisfatória")
-        # final_report_container.write("Zona de transformação: N/A")
         final_report_container.write("Organismos: Detectáveis")
-        # final_report_container.write(f"Quantidade HSIL: {hsil}")
-        # final_report_container.write(f"Quantidade LSIL: {lsil}")
-        # final_report_container.write(f"Quantidade NORMAL: {normal}")
-        hsil_rate = hsil / (lsil+normal)
-        lsil_rate = lsil / (hsil+normal)
-        normal_rate = normal / hsil + lsil
-        print(hsil, lsil, normal)
-        print(hsil_rate, lsil_rate, normal_rate)
-        if hsil_rate >= (lsil_rate + normal_rate) :
-            final_report_container.warning("Interpretação e Resultado: Possível malignidade HSIL")
-            final_report_container.warning("Complexidade: Alta complexidade")
-            final_report_container.warning("Sugestivo: GRUPO 4")
-        elif lsil_rate >= (hsil_rate + normal_rate):
-            final_report_container.warning("Interpretação e Resultado: Possível malignidade LSIL")
-            final_report_container.warning("Complexidade: Baixa complexidade")
-            final_report_container.warning("Sugestivo: GRUPO 3")
-        else:
-            final_report_container.warning("Interpretação e Resultado: Possível NEGATIVO")
-            final_report_container.warning("Complexidade: Baixa Complexidade")
-            final_report_container.warning("Sugestivo: GRUPO 1 e 2")
-
-    else:
-        for classes in report:
-                for cell in classes:
-                    if cell == 0:
-                        hsil += 1
-                    elif cell == 1:
-                        lsil += 1
-                    else:
-                        normal += 1
+        hsil = classes_amount.get('HSIL')
+        lsil = classes_amount.get('LSIL')
+        normal = classes_amount.get('NORMAL')
+        final_report_container.text("=====================================================")
+        calculate_rates_opt1(hsil, lsil, normal)
+        final_report_container.text("=====================================================")
+        calculate_rates_opt2(hsil, lsil, normal)
+        final_report_container.text("=====================================================")
+        calculate_rates_opt3(hsil, lsil, normal)
+        final_report_container.text("=====================================================")
+        
+        
             
 st.set_page_config(page_title="PickCells", page_icon='icon.png', layout='wide')
 st.image(image='pickcells-logo.png')
@@ -90,6 +132,7 @@ with col2.container(height=700, border=False):
         if len(uploaded_files) == 0:
             final_report_container.warning('Você precisa submeter ao menos um arquivo para continuar.')
         else:
+            expand = st.expander('')
             progress_bar = st.progress(0, text="Analisando... Por favor aguarde.")
             init_time_analysis = datetime.datetime.now()
             st.write(f'Início da análise às: {(init_time_analysis - datetime.timedelta(hours=3)).strftime("%H:%M:%S")}')
