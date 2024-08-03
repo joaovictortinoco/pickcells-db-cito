1c8e6851eb3e8f4ae48b4206028b8a12a44e0448
joaovictortinoco <joaovictortinoco@gmail.com>
2024-08-03 12:57:34 -0300

    Add title to get path on server

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import asyncio
 from pathlib import Path
 from PIL import Image
 import datetime
+import os
 
 def analyze_classes_from_prediction(report, final_report: bool | None = False):
     hsil = 0
@@ -59,7 +60,7 @@ def analyze_classes_from_prediction(report, final_report: bool | None = False):
 st.set_page_config(page_title="PickCells", page_icon='icon.png', layout='wide')
 st.image(image='pickcells-logo.png')
 title_col1,mid, title_col2 = st.columns([1,2,35])
-
+st.title(os.getcwd())
 col1, col2 = st.columns(2, gap='large')
 
 with col1:
